cade29dc77d0ce48ccc4f616c176c5ea914d85a2
Moncayo1329 <moncayomichael23@gmail.com>
2026-09-12 10:48:25 +0100

    still working front page

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,12 +1,16 @@
-import { Text } from 'react-native';
+import { Text, View } from 'react-native';
 import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
 
 const HomeScreen = () => {
   return (
     <SafeAreaProvider>
       <SafeAreaView>
+        <View>
         <Text>Carguita</Text>
-        <Text>PRUEBA 123333</Text>
+         <Text>Carguita</Text>
+          <Text>Carguita</Text>
+          <Text>PRUEBA 123333</Text>
+        </View>
       </SafeAreaView>
     </SafeAreaProvider>
   );
